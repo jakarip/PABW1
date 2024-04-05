c239ede6ce8c373e27f956fcfd24424c5a4e8295
jakarip <dzakiarief42@gmail.com>
2024-04-05 14:25:56 +0700

    1.6

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function checkAvailability(){
+
+    let buku = [
+        {"nama": "sherlock", "jumlah":2},
+        {"nama": "lupin", "jumlah":2},
+        {"nama": "aladin", "jumlah":2},
+      ]
     
     let jumlahInput = document.getElementById("jumlah").value;
 
@@ -8,11 +14,22 @@ function checkAvailability(){
         alert("Please enter the quantity.")
         return;
     }
+    else{
+        let found = false;
+
+        for (let i = 0; i < buku.length; i++) {
+            if (buku[i].nama === bukuSelect){
+                found = true;
+                break;
+            }
+            
+        }
+        if (!found){
+            alert("Book not found.");
+            return;
+        }
+    }
 }
 
-let buku = [
-    {"nama": "sherlock", "jumlah":2},
-    {"nama": "lupin", "jumlah":2},
-    {"nama": "aladin", "jumlah":2},
-  ]
+
 
