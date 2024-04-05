b638724c0e7368804908549a84838a17b58373e4
jakarip <dzakiarief42@gmail.com>
2024-04-05 14:14:18 +0700

    1.4

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ function checkAvailability(){
     let bukuSelect = document.getElementById("buku").value;
 }
 
+let buku = [
+    {"nama": "sherlock", "jumlah":2},
+    {"nama": "lupin", "jumlah":2},
+    {"nama": "aladin", "jumlah":2},
+  ]
